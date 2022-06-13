6b3420080fb6b0b19397c0ade70f2e2619475eb4
Jonathan Guyot Bourgeois <j.guyot-bourgeois@libr.im>
2022-06-13 17:29:25 +0200

    create test for issue #4

--- src/tests/test_purchase_places.py
+++ src/tests/test_purchase_places.py
@@ -22,3 +22,17 @@ def test_purchase_places_with_more_points_than_available(
     data = response.data.decode()
     assert response.status_code == 200
     assert 'You do not have enough points' in response.data.decode()
+
+
+def test_purchase_places_over_book_limit(
+    mocker, client, config, club, competition
+):
+    mocker.patch('server.MAX_BOOK', 1)
+    data = {
+        'club': club['name'],
+        'competition': competition['name'],
+        'places': f"{2}",
+    }
+    response = client.post('purchase-places', data=data)
+    assert response.status_code == 200
+    assert 'You cannot purchase more than' in response.data.decode()
